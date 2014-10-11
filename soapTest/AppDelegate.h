fb8b38ecb96f7ca651ac399ddce8571222687f56
gongke <gongke@bingosoftyc-3.local>
2014-10-11 16:03:37 +0800

    Initial Commit

---
+++ soapTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  soapTest
+//
+//  Created by gongke on 14-10-11.
+//  Copyright (c) 2014年 gongke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ soapTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  soapTest
+//
+//  Created by gongke on 14-10-11.
+//  Copyright (c) 2014年 gongke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
